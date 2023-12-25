74720bc1c998cdf493c77b434f8ac7714c5bf722
RowMur <rowanomurray@gmail.com>
2023-12-25 01:03:10 +0000

    feat: add inspect command

--- cli.go
+++ cli.go
@@ -49,6 +49,11 @@ func getCliCommands() map[string]CliCommand {
 			description: "Attempts to catch a pokemon",
 			callback: commandCatch,
 		},
+		"inspect": {
+			name: "inspect",
+			description: "Inspects a pokemon from the pokedex",
+			callback: commandInspect,
+		},
 	}
 }
 
@@ -175,6 +180,34 @@ func commandCatch(state *CliState, cache *cacheType, commandParams []string) err
 	pokedex := *state.pokedex
 	entry := pokedex[p.Name]
 	fmt.Printf("%s was caught! Caught a total of %v times\n", entry.pokemon.Name, entry.timesCaught)
+	return nil
+}
+
+func commandInspect(state *CliState, cache *cacheType, commandParams []string) error {
+	pokemonToInspect := commandParams[0]
+	pokedex := *state.pokedex
+
+	pokedexEntry, ok := pokedex[pokemonToInspect]
+	if !ok {
+		fmt.Printf("you have not caught %s yet!\n", pokemonToInspect)
+		return nil
+	}
+
+	pokemon := pokedexEntry.pokemon
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %v\n", pokemon.Height)
+	fmt.Printf("Weight: %v\n", pokemon.Weight)
+
+	fmt.Printf("Stats:\n")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf(" - %s: %v\n", stat.Stat.Name, stat.BaseStat)
+	}
+
+	fmt.Printf("Types:\n")
+	for _, pokemonType := range pokemon.Types {
+		fmt.Printf(" - %s\n", pokemonType.Type.Name)
+	}
+
 	return nil
 }
 
